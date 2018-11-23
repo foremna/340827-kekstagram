5ee591d62da8a4eb49dabdc866132080a49aa2c7
foremna <missmoxi2011@yandex.ru>
2018-11-23 17:14:38 +0300

    Отрисовка фото на странице

--- js/pictures.js
+++ js/pictures.js
@@ -2,15 +2,13 @@
 
 var commentaries = ['Всё отлично! В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
-var tag = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......','Вот это тачка!']
-
-var ESC_KEYCODE = 27;
+var tag = ['Тестим новую камеру!', 'Затусили с друзьями на море', 'Как же круто тут кормят', 'Отдыхаем...', 'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......', 'Вот это тачка!'];
 
 var getRandomInRange = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getRandElement = function (tag) {
+var getRandElement = function () {
   var rand = Math.floor(Math.random() * tag.length);
   return tag[rand];
 };
@@ -43,10 +41,10 @@ var getSidawaysDataArray = function () {
       likes: getRandomInRange(15, 200), // лайки карточки
       comments: getRandomTastes(commentaries), // описание карточки
       description: getRandElement(tag) // тэг карточки
-    }
+    };
   }
   return picture;
-}
+};
 
 var picture = getSidawaysDataArray();
 
@@ -66,7 +64,7 @@ var createPictures = function () {
     pictureLikes.textContent = picture[i].likes; // на каждой иттерации цикла берем из объекта значение likes-лайки из массива picture
     pictureComments.textContent = picture[i].comments; // на каждой иттерации цикла берем из объекта значение comments-описание из массива picture
     fragment.appendChild(pisturesCards);
-  };
+  }
 
   containerPictures.appendChild(fragment); // отрисовывает в контейнере карточки
 };
@@ -77,19 +75,19 @@ var openPhoto = document.querySelector('.big-picture'); // блок открыт
 // openPhoto.classList.remove('hidden'); // убирает скрывающий класс у блока открытой фотографии // ВРЕМЕННО ЗАКРЫТ, ПОКА РАЗРАБАТЫВАЕТСЯ КОД ЗАГРУЗКИ ФОТО
 var openPhotoImage = openPhoto.querySelector('.big-picture__img'); // фото открытой фотографии
 var openPhotoLikes = openPhoto.querySelector('.likes-count'); // лайки открытой фотографии
-var openPhotoComments = openPhoto.querySelector('.comments-count'); // описание открытой фотографии
+// var openPhotoDescribe = openPhoto.querySelector('.comments-count'); // описание открытой фотографии
 var openPhotoCaption = openPhoto.querySelector('.social__caption'); // комментарий открытой фотографии
 
 var openPhotoSocialComments = openPhoto.querySelector('.social__comments'); // блок с комментариями
-var openPhotoComments = openPhotoSocialComments.querySelector('.social__comment'); // комментарий
+var openPhotoComment = openPhotoSocialComments.querySelector('.social__comment'); // комментарий
 var openPhotoCommentsCaption = openPhoto.querySelector('.social__comment-count'); // счётчик комментариев
 var openPhotoCommentsLoader = openPhoto.querySelector('.comments-loader'); // загрузка новых комментариев
 
 var createOpenPhoto = function () {
   openPhotoImage.src = picture[1].url;
   openPhotoLikes.textContent = picture[1].likes;
-  openPhotoComments.textContent = picture[1].comments;
-  openPhotoSocialComments.innerHTML = '<li class=\"social__comment\"><img class=\"social__picture\" src=\"img/avatar-' + getRandomInRange(1, 6) + '.svg\" alt=\"Аватар комментатора фотографии\" width=\"35\" height=\"35\"><p class=\"social__text\">' + picture[1].comments + '</p></li>'
+  openPhotoComment.textContent = picture[1].comments;
+  openPhotoSocialComments.innerHTML = '<li class=\"social__comment\"><img class=\"social__picture\" src=\"img/avatar-' + getRandomInRange(1, 6) + '.svg\" alt=\"Аватар комментатора фотографии\" width=\"35\" height=\"35\"><p class=\"social__text\">' + picture[1].comments + '</p></li>';
   openPhotoCaption.innerHTML = picture[1].description; // добавляет комментарии из массива
 };
 
@@ -101,3 +99,5 @@ var hidesClasses = function () {
 };
 
 hidesClasses();
+
+
